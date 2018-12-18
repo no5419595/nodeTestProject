6a1908339573852a70178ee5ee6ad82aaf285ef3
Grace Kim <gracekim@ca.ibm.com>
2018-12-17 21:23:18 -0500

    src

--- src/database/db.js
+++ src/database/db.js
@@ -1,44 +1,125 @@
 const dataFile = require('./data/data.json');
+var fs = require('fs');
 const pageSize = 10;
 
 module.exports = {
+
     all: function(){
         return dataFile;
     },
 
     pagination: function(data, pageNumber){
-        console.log('pageNumber:', pageNumber);
         if(data.length > pageSize){
             data = data.slice((pageNumber -1) * pageSize, pageNumber * pageSize);
         }
         return data;
     },
 
-    sortData: function(data, sort){
-        let criterias = sort[0];
-        for (var field in criterias) {
-            console.log("sort by field:" , field, " : " , criterias[field]);
-
-            if(field=="first_name" || field=="last_name" || field=="email" || field=="gender"){
-                if(criterias[field]=="asc"){
-                    data= data.sort(function(a, b){
-                        if(a[field] < b[field]) { return -1; }
-                        if(a[field] > b[field]) { return 1; }
-                        return 0;
-                    });
-                }else if(criterias[field]=="desc"){
-                    data =data.sort(function(a, b) {return b[field] - a[field]});
-                } 
-            }else if (field=="age" || field=="id"){       
-                if(criterias[field]=="asc"){
-                    data =data.sort(function(a, b) {return a[field] - b[field]});
-                }else if(criterias[field]=="desc"){
-                    data =data.sort(function(a, b) {return b[field] - a[field]});
-                }                 
-            }
-        }
-        return data;
+    addData: function(data){
+        //append id to the new data
+        lastData = dataFile[dataFile.length-1];
+        data.id= lastData.id +1;
+       
+        dataFile.push(data);
+        fs.writeFile(__dirname+ "/data/data.json", JSON.stringify(dataFile), function (err) {
+            if (err) throw 400;
+            return 201;
+        });
     },
+
+    // sortData: function(data, sort){
+    //     for(let i=0; i < sort.length; i++){
+    //         let field = sort[i].split('|');
+    //         console.log("sort by field:" , field);
+            
+    //         if(field[0]=="first_name" || field[0]=="last_name" || field[0]=="email" || field[0]=="gender"){
+    //             if(field[1]=="asc"){
+    //                 data= data.sort(function(a, b){
+    //                     if(a[field[0]] < b[field[0]]) { return -1; }
+    //                     if(a[field[0]] > b[field[0]]) { return 1; }
+    //                     return 0;
+    //                 });
+    //             }else if(field[1]=="desc"){
+    //                 data= data.sort(function(a, b){
+    //                     if(a[field[0]] > b[field[0]]) { return -1; }
+    //                     if(a[field[0]] < b[field[0]]) { return 1; }
+    //                     return 0;
+    //                 });
+    //             } 
+    //         }else if (field[0]=="age" || field[0]=="id"){       
+    //             if(field[1]=="asc"){
+    //                 data =data.sort(function(a, b) {return a[field[0]] - b[field[0]]});
+    //             }else if(field[1]=="desc"){
+    //                 data =data.sort(function(a, b) {return b[field[0]] - a[field[0]]});
+    //             }                 
+    //         }
+    //     }
+    //     return data;
+    // },
+
+    // filterData:function(data, filters){
+    //     console.log('filters:', filters);
+    //     let resultSet = [];
+
+    //     for(let k=0; k < data.length; k++){
+    //         let currentData = data[k];
+    //         console.log('currentData:', currentData);
+
+    //         let addData = true;
+    //         for(let i = 0; i < filters.length; i++) {
+    //             var filter = filters[i];        
+    //             console.log('filter: ', filter);
+    //             filter= filter.split('|');
+    //             console.log('splitted filter: ', filter);
+    //             if(filter[0]=="first_name" || filter[0]=="last_name" || filter[0]=="email" || filter[0]=="gender"){
+    //                 switch(filter[1]) {
+    //                     case "STARTSWITH":
+    //                         console.log('operator: ', filter[1]);
+    //                         if(currentData[filter[0]].indexOf(filter[2])!=0){
+    //                             addData=false;
+    //                         }
+    //                         console.log('addData?: ', addData);
+    //                         break;
+    //                     case "CONTAINS":
+    //                     console.log('operator: ', filter[1]);
+    //                     if(currentData[filter[0]].indexOf(filter[2])==-1){
+    //                             addData=false;
+    //                         }
+    //                         console.log('addData?: ', addData);
+    //                       break;
+    //                 }
+    //             }else if (filter[0]=="age" || filter[0]=="id"){
+    //                 switch(filter.operator) {
+    //                     case "GREAT-THAN":
+    //                         console.log('operator: ', filter[1]);
+    //                         if(currentData[filter[0]]<= filter[2]){
+    //                             addData=false;
+    //                         }
+    //                         console.log('addData?: ', addData);
+    //                         break;
+    //                     case "EQUALS":
+    //                         console.log('operator: ', filter[1]);
+    //                         if(currentData[filter[0]]!= filter[2]){
+    //                             addData=false;
+    //                         }
+    //                         console.log('addData?: ', addData);
+    //                       break;
+    //                     case "LESS-THAN":
+    //                         console.log('operator: ', filter[1]);
+    //                         if(currentData[filter[0]]>=filter[2]){
+    //                             addData=false;
+    //                         }
+    //                         console.log('addData?: ', addData);
+    //                         break;
+    //                 }
+    //             }
+    //         }
+    //         if(addData){
+    //             resultSet.push(currentData);
+    //         }
+    //     }
+    //     return resultSet;
+    // }
 
     filterData:function(data, filters){
         console.log('filters:', filters);
@@ -100,5 +181,31 @@ module.exports = {
             }
         }
         return resultSet;
+    },
+
+    sortData: function(data, sort){
+        let criterias = sort[0];
+        for (var field in criterias) {
+            console.log("sort by field:" , field, " : " , criterias[field]);
+
+            if(field=="first_name" || field=="last_name" || field=="email" || field=="gender"){
+                if(criterias[field]=="asc"){
+                    data= data.sort(function(a, b){
+                        if(a[field] < b[field]) { return -1; }
+                        if(a[field] > b[field]) { return 1; }
+                        return 0;
+                    });
+                }else if(criterias[field]=="desc"){
+                    data =data.sort(function(a, b) {return b[field] - a[field]});
+                } 
+            }else if (field=="age" || field=="id"){       
+                if(criterias[field]=="asc"){
+                    data =data.sort(function(a, b) {return a[field] - b[field]});
+                }else if(criterias[field]=="desc"){
+                    data =data.sort(function(a, b) {return b[field] - a[field]});
+                }                 
+            }
+        }
+        return data;
     }
 }
--- src/server.js
+++ src/server.js
@@ -1,57 +1,71 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const cors = require('cors');
 const database = require('./database/db');
-const EventEmitter = require('events').EventEmitter;
-
-var corsOptions = {
-    origin: 'http://localhost:4200',
-    optionsSuccessStatus: 200
-  }
 
 
-//CORS
-app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.listen(8000, () => {
     console.log('Server started!');
-});
+}); 
 
 
 //REST API
-app.get('/', function(req, res) {
+// app.get('/', function(req, res) {
+//     res.setHeader('Content-Type', 'application/json');
+
+//     //grab all data
+//     let data = database.all();
+
+//     //filtering is done first since it will reduce the number of data processed later
+//     if(req.query.filters){
+//         let filters= req.query.filters.split(',');
+//         data = database.filterData(data,filters);
+//     }
+
+//     //sort
+//     if(req.query.sort){
+//         let sorting= req.query.sort.split(',');
+//         data= database.sortData(data, sorting);
+//     }
+
+//     //pagination
+//     if(req.query.pagenumber){
+//         data= database.pagination(data, req.query.pagenumber);
+//     }
+
+//     res.send(data);
+
+// });
+
+app.post('/', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
 
-    let data = database.all();
+    console.log('body is ',req.body);
+    let body = req.body;
 
-    console.log('req.query:', req.query);
+    if(body.query==true){
+        //grab all data
+        let data = database.all();
 
-    //filter
-    // if(true){   
-    //     let filterjson= [
-    //         {"field": "first_name", "operator": "STARTSWITH", "value": "Sar"},
-    //         {"field": "age", "operator": "GREATER-THAN", "value": 21},
-    //         {"field": "last_name", "operator": "CONTAINS", "value": "Doyle"}
-    //     ]
-    //     data = database.filterData(data,filterjson);
-    //     console.log('data22222?', data);
-    // }
+        //filtering is done first since it will reduce the number of data processed later
+        if(body.filters){
+            data = database.filterData(data,body.filters);
+        }
 
-    //sorting
-    if(true){
-        let sortJson=[{"first_name": "asc", "last_name": "desc", "age": "asc"}];
-        // let sortJson=[{"first_name": "asc"}];
+        //sort
+        if(body.sort){
+            data= database.sortData(data, body.sort);
+        }
 
-        data= database.sortData(data, sortJson);
-        // console.log('data33?', data);
+        //pagination
+        if(req.query.pagenumber ){
+            data= database.pagination(data, req.query.pagenumber);
+        }
+
+        res.send(data);
+    }else if (body.query==add){
+        res.send(database.addData(req.body));
+        res.send();
     }
-
-    //pagination
-    if(req.query.pagenumber){
-        data= database.pagination(data, req.query.pagenumber);
-    }
-
-    res.send(data);
-
 });
